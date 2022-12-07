846ed9c0b5ec04525dfdb394b95b4dc33eb597bf
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-08 00:11:13 +0300

    quick sort

---
+++ Algorithms/Algorithms/04_QuickSort.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal static class QuickSort
+    {
+        internal static void QuickSortSample(IList<int> list, int method = 1)
+        {
+            var js = JsonSerializer.SerializeToDocument(list, list.GetType());
+            Console.WriteLine($"Source array: {js.RootElement}");
+            var newList = method == 1 ? QuickSort1(list) : QuickSort2(list);
+            js = JsonSerializer.SerializeToDocument(newList, newList.GetType());
+            Console.WriteLine($"Target array: {js.RootElement}");
+            Console.WriteLine();
+        }
+
+        internal static IList<int> QuickSort1(IList<int> list)
+        {
+            if (list.Count < 2)
+                return list;
+            else
+            {
+                var pivot = list[list.Count / 2];
+                var less = new List<int>();
+                var greater = new List<int>();
+                var equal = new List<int>();
+
+                foreach (var x in list)
+                {
+                    if (x < pivot)
+                        less.Add(x);
+                    else if (x > pivot)
+                        greater.Add(x);
+                    else
+                        equal.Add(x);
+                }
+
+                return QuickSort1(less).Concat(equal).Concat(QuickSort1(greater)).ToList();
+            }
+        }
+
+        internal static IList<int> QuickSort2(IList<int> list)
+        {
+            var newList = new List<int>();
+
+            return newList;
+        }
+
+    }
+}
--- Algorithms/Algorithms/Program.cs
+++ Algorithms/Algorithms/Program.cs
@@ -15,12 +15,14 @@ namespace Algorithms
                 //BinarySearch.BinarySearchSample();
 
                 //SelectionSort.SelectionSortSample(new List<int> { 7, 3, 5, 1, 9 });
-                //SelectionSort.SelectionSortSample(new List<int> { 7, 3, 5, 1, 9 });
+                //SelectionSort.SelectionSortSample(new List<int> { 7, 3, 5, 1, 9 }, 2);
 
                 //Console.WriteLine($"Factorial 0: {SpecialFunctions.Factorial(0)}");
                 //Console.WriteLine($"Factorial 4: {Factorial.Factorial1(4)}");
                 //Console.WriteLine($"Factorial 5: {Factorial.Factorial2(5)}");
-                Console.WriteLine($"Factorial 5: {Factorial.Factorial3(5)}");
+                //Console.WriteLine($"Factorial 5: {Factorial.Factorial3(5)}");
+
+                QuickSort.QuickSortSample(new List<int> { 7, 3, 5, 1, 9 });
             }
             catch (Exception ex)
             {
