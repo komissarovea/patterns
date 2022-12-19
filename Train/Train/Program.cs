c8f269b4db71af5465ea1adee6924e91d2b417f0
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 23:08:23 +0300

    closure 1

--- Train/Train/Program.cs
+++ Train/Train/Program.cs
@@ -1,44 +1,29 @@
-﻿class Message
-{
-    public string Text { get; }
-    public Message(string text) => Text = text;
-    public virtual void Print() => Console.WriteLine($"Message: {Text}");
-}
-class EmailMessage : Message
-{
-    public EmailMessage(string text) : base(text) { }
-    public override void Print() => Console.WriteLine($"Email: {Text}");
-}
-class SmsMessage : Message
-{
-    public SmsMessage(string text) : base(text) { }
-    public override void Print() => Console.WriteLine($"Sms: {Text}");
-}
-
+﻿
 class Program
 {
-
-    delegate Message MessageBuilder(string text);
-    delegate void EmailReceiver(EmailMessage message);
-
     static void Main(string[] args)
     {
-        // делегату с базовым типом передаем метод с производным типом
-        MessageBuilder messageBuilder = WriteEmailMessage; // ковариантность
-        Message message = messageBuilder("Hello");
-        message.Print();    // Email: Hello
+        var fn = Outer();   // fn = Inner, так как метод Outer возвращает функцию Inner
+                            // вызываем внутреннюю функцию Inner
+        fn();   // 6
+        fn();   // 7
+        fn();   // 8
 
-
-        EmailReceiver emailBox = ReceiveMessage; // контравариантность
-        emailBox(new EmailMessage("Welcome"));  // Email: Welcome
-
+        Action Outer()  // метод или внешняя функция
+        {
+            int x = 5;  // лексическое окружение - локальная переменная
+            void Inner()    // локальная функция
+            {
+                x++;        // операции с лексическим окружением
+                Console.WriteLine(x);
+            }
+            return Inner;   // возвращаем локальную функцию
+        }
 
         Console.Read();
     }
 
-    static EmailMessage WriteEmailMessage(string text) => new EmailMessage(text);
 
-    static void ReceiveMessage(Message message) => message.Print();
 
 
 }
