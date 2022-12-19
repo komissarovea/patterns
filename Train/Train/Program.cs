3b795b304f3a0aab01cf3461ae4ca7d5f9410abf
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 21:49:57 +0300

    IFoo.Execute()

--- Train/Train/Program.cs
+++ Train/Train/Program.cs
@@ -13,20 +13,48 @@ public interface IAction
 }
 internal interface IRunAction : IAction
 {
-     new void Move();
+    new void Move();
+}
+
+interface IFoo
+{
+    void Execute();
+}
+interface IBar
+{
+    void Execute();
+}
+class Tester : IFoo, IBar
+{
+    public void Execute()
+    {
+        Console.WriteLine("Tester Executes");
+    }
+
+    void IFoo.Execute()
+    {
+        Console.WriteLine("IFoo Executes");
+    }
+    void IBar.Execute()
+    {
+        Console.WriteLine("IBar Executes");
+    }
 }
 
 class Program
 {
     static void Main(string[] args)
     {
+        Tester t = new Tester();
+        t.Execute();
+
+        ((IBar)t).Execute();
+        (t as IFoo).Execute();
 
         IAction action = new RunAction();
         action.Move();
 
         Console.ReadKey();
     }
-
-
 }
 
