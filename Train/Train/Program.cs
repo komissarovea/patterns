e0927357e960b680293b96ac3ef02da4886544c2
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 15:09:39 +0300

    fix restrictive prop

--- Train/Train/Program.cs
+++ Train/Train/Program.cs
@@ -1,13 +1,13 @@
 ﻿class Person
 {
-    internal string Name { internal get; set; } = "Bob";
+    internal string Name { private get; set; } = "Bob";
 }
 class Program
 {
     static void Main(string[] args)
     {
-        Person tom = new Person { Name = "Tom" };
-        Console.WriteLine(tom.Name);
+        //Person tom = new Person { Name = "Tom" };
+        //Console.WriteLine(tom.Name);
 
         Console.ReadKey();
     }
