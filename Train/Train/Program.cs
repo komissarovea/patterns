ef644cd1aa208345bcca4d9952052ecaa4087a27
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 23:24:01 +0300

    variant interface

--- Train/Train/Program.cs
+++ Train/Train/Program.cs
@@ -1,24 +1,46 @@
-﻿
+﻿class Message
+{
+    public string Text { get; set; }
+    public Message(string text) => Text = text;
+}
+class EmailMessage : Message
+{
+    public EmailMessage(string text) : base(text) { }
+}
+
+interface IMessenger<in T, out K>
+{
+    void SendMessage(T message); // Контравариантность
+    K WriteMessage(string text); //Ковариантность
+}
+class SimpleMessenger : IMessenger<Message, EmailMessage>
+{
+    public void SendMessage(Message message)
+    {
+        Console.WriteLine($"Отправляется сообщение: {message.Text}");
+    }
+    public EmailMessage WriteMessage(string text)
+    {
+        return new EmailMessage($"Email: {text}");
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        var fn = Outer();   // fn = Inner, так как метод Outer возвращает функцию Inner
-                            // вызываем внутреннюю функцию Inner
-        fn();   // 6
-        fn();   // 7
-        fn();   // 8
+        IMessenger<EmailMessage, Message> messenger = new SimpleMessenger();
+        EmailMessage message = messenger.WriteMessage("Hello World"); //Ковариантность
+        Console.WriteLine(message.Text);
+        messenger.SendMessage(new EmailMessage("Test"));
 
-        Action Outer()  // метод или внешняя функция
-        {
-            int x = 5;  // лексическое окружение - локальная переменная
-            void Inner()    // локальная функция
-            {
-                x++;        // операции с лексическим окружением
-                Console.WriteLine(x);
-            }
-            return Inner;   // возвращаем локальную функцию
-        }
+        IMessenger<EmailMessage, EmailMessage> outlook = new SimpleMessenger();
+        EmailMessage emailMessage = outlook.WriteMessage("Message from Outlook");
+        outlook.SendMessage(emailMessage);
+
+        IMessenger<Message, Message> telegram = new SimpleMessenger();
+        Message simpleMessage = telegram.WriteMessage("Message from Telegram");
+        telegram.SendMessage(simpleMessage);
 
         Console.Read();
     }
