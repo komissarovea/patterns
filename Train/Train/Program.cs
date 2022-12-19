39ff40b6d4c04c0a161511f0e5dea97086140342
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 21:47:04 +0300

    new method in interface

--- Train/Train/Program.cs
+++ Train/Train/Program.cs
@@ -1,62 +1,30 @@
-﻿class Person<T> 
+﻿public class RunAction : IRunAction
 {
-    public T Id { get; }
-    public Person(T id)
+    public void Move()
     {
-        Id = id;
+        Console.WriteLine("I am running");
     }
+
 }
 
-class UniversalPerson<T> : Person<T>
+public interface IAction
 {
-    public UniversalPerson(T id) : base(id) { }
+    void Move();
 }
-
-
-class StringPerson : Person<string>
+internal interface IRunAction : IAction
 {
-    public StringPerson(string id) : base(id) { }
-}
-
-class IntPerson<T> : Person<int>
-{
-    public T Code { get; set; }
-    public IntPerson(int id, T code) : base(id)
-    {
-        Code = code;
-    }
-}
-
-class MixedPerson<T, K> : Person<T>
-    where K : struct
-{
-    public K Code { get; set; }
-    public MixedPerson(T id, K code) : base(id)
-    {
-        Code = code;
-    }
+     new void Move();
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        StringPerson person4 = new StringPerson("438767");
-        Person<string> person5 = new StringPerson("43875");
-        // так нельзя написать
-        //Person<int> person6 = new StringPerson("45545");
-        Console.WriteLine(person4.Id);
-        Console.WriteLine(person5.Id);
 
-        IntPerson<string> person7 = new IntPerson<string>(5, "r4556");
-        Person<int> person8 = new IntPerson<long>(7, 4587);
-        Console.WriteLine(person7.Id);
-        Console.WriteLine(person8.Id);
+        IAction action = new RunAction();
+        action.Move();
 
-        MixedPerson<string, int> person9 = new MixedPerson<string, int>("456", 356);
-        Person<string> person10 = new MixedPerson<string, int>("9867", 35678);
-        Console.WriteLine(person9.Id);
-        Console.WriteLine(person10.Id);
+        Console.ReadKey();
     }
 
 
