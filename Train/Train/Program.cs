9da113d772c7fff1e19d492072e1d29375573aec
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 21:57:44 +0300

    null delegate

--- Train/Train/Program.cs
+++ Train/Train/Program.cs
@@ -1,24 +1,16 @@
 ﻿class Program
 {
-    delegate int Operation(int x, int y);
+    delegate void Message();
 
     static void Main(string[] args)
     {
-        Operation del = Add;
-        del += Multiply;
-
-        int result = del(6, 5);
-        Console.WriteLine(result);
+        Message mes = new Message(Hello);
+        mes += Hi;
+        mes -= Hello;
+        mes -= Hi;
+        mes();
         Console.Read();
     }
-    private static int Add(int x, int y) 
-    {
-        Console.WriteLine("Add");
-        return x + y;
-    }
-    private static int Multiply(int x, int y) 
-    {
-        Console.WriteLine("Multiply");
-        return x * y;
-    }
+    private static void Hello() { Console.WriteLine("Hello"); }
+    private static void Hi() { Console.WriteLine("Hi"); }
 }
