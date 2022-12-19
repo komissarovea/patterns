9ee49f664ba9334ee78f70bf61487eb7a9be777f
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 14:09:44 +0300

    in parameters

--- Train/Train/Program.cs
+++ Train/Train/Program.cs
@@ -1,17 +1,13 @@
-﻿class Program
+﻿void GetRectangleData(in int width, in int height, out int rectArea, out int rectPerimetr)
 {
-    static void Main(string[] args)
-    {
-        int i = 0;
-        Console.Write(i++ * Calculate(i));
-        Console.WriteLine(i);
-
-        Console.ReadKey();
-    }
-
-    public static int Calculate(int i)
-    {
-        Console.Write(i++);
-        return i;
-    }
+    //width = 25; // нельзя изменить, так как width - входной параметр
+    rectArea = width * height;
+    rectPerimetr = (width + height) * 2;
 }
+
+int w = 10;
+int h = 20;
+GetRectangleData(w, h, out var area, out var perimetr);
+
+Console.WriteLine($"Площадь прямоугольника: {area}");       // 200
+Console.WriteLine($"Периметр прямоугольника: {perimetr}");   // 60
