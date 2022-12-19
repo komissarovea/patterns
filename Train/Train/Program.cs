6d36ef0cbd90994caca2309f8e92c5d6ad9a0349
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 13:06:00 +0300

    checked sample

---
+++ Train/Train/Program.cs
@@ -0,0 +1,13 @@
+﻿int j = 2;
+for (int i = 2; i < 32; i = i * 2)
+{
+    while (i < j)
+    {
+        j = checked((int)(j * 2));
+        Console.WriteLine(j);
+    }
+    i = j - i;
+}
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
