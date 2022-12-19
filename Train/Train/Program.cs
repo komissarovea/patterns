c580df3439d26a3ca9a285a1172dcdeefe212747
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 21:04:28 +0300

    generic method

--- Train/Train/Program.cs
+++ Train/Train/Program.cs
@@ -1,22 +1,13 @@
-﻿class Person<T>
+﻿class Person<T, K>
 {
-    public static T? code;
-
-    public T Id { get; set; }
-    public string Name { get; set; }
-    public Person(T id, string name)
+    public T Id { get; }
+    public K Password { get; set; }
+    public string Name { get; }
+    public Person(T id, K password, string name)
     {
         Id = id;
         Name = name;
-    }
-}
-
-class Company<P>
-{
-    public P CEO { get; set; }  // президент компании
-    public Company(P ceo)
-    {
-        CEO = ceo;
+        Password = password;
     }
 }
 
@@ -24,19 +15,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Person<int> tom = new Person<int>(546, "Tom");  // упаковка не нужна
-        Person<string> bob = new Person<string>("abc123", "Bob");
+        Person<int, string> tom = new Person<int, string>(546, "qwerty", "Tom");
+        Console.WriteLine(tom.Id);  // 546
+        Console.WriteLine(tom.Password);// qwerty
 
-        int tomId = tom.Id;      // распаковка не нужна
-        string bobId = bob.Id;  // преобразование типов не нужно
-        Company<Person<int>> microsoft = new Company<Person<int>>(tom);
+        int x = 7;
+        int y = 25;
+        Swap<int>(ref x, ref y); // или так Swap(ref x, ref y);
+        Console.WriteLine($"x={x}    y={y}");   // x=25   y=7
 
+        string s1 = "hello";
+        string s2 = "bye";
+        Swap<string>(ref s1, ref s2); // или так Swap(ref s1, ref s2);
+        Console.WriteLine($"s1={s1}    s2={s2}"); // s1=bye   s2=hello
+    }
 
-        Person<int>.code = 1234;
-        Person<string>.code = "meta";
-
-        Console.WriteLine(Person<int>.code);       // 1234
-        Console.WriteLine(Person<string>.code);   // meta
+    static void Swap<T>(ref T x, ref T y)
+    {
+        T temp = x;
+        x = y;
+        y = temp;
     }
 }
 
