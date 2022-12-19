dfee79dc4c85f68537c2d191e63b34569108ccd0
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 23:28:07 +0300

    Анонимные типы

--- Train/Train/Program.cs
+++ Train/Train/Program.cs
@@ -1,53 +1,20 @@
-﻿class Message
+﻿class Auto
 {
-    public string Text { get; set; }
-    public Message(string text) => Text = text;
+    public string Name { get; set; }
 }
-class EmailMessage : Message
+class User
 {
-    public EmailMessage(string text) : base(text) { }
+    public string Name { get; set; }
+    public Auto Auto { get; set; }
 }
-
-interface IMessenger<in T, out K>
-{
-    void SendMessage(T message); // Контравариантность
-    K WriteMessage(string text); //Ковариантность
-}
-class SimpleMessenger : IMessenger<Message, EmailMessage>
-{
-    public void SendMessage(Message message)
-    {
-        Console.WriteLine($"Отправляется сообщение: {message.Text}");
-    }
-
-    public EmailMessage WriteMessage(string text)
-    {
-        return new EmailMessage($"Email: {text}");
-    }
-}
-
 class Program
 {
     static void Main(string[] args)
     {
-        IMessenger<EmailMessage, Message> messenger = new SimpleMessenger();
-        Message message = messenger.WriteMessage("Hello World");
-        Console.WriteLine(message.Text);
-        messenger.SendMessage(new EmailMessage("Test")); // Контравариантность
-
-        IMessenger<EmailMessage, EmailMessage> outlook = new SimpleMessenger();
-        EmailMessage emailMessage = outlook.WriteMessage("Message from Outlook");
-        outlook.SendMessage(emailMessage); // Контравариантность
-
-        IMessenger<Message, Message> telegram = new SimpleMessenger();
-        Message simpleMessage = telegram.WriteMessage("Message from Telegram");
-        telegram.SendMessage(simpleMessage); // Контравариантность
-
-        // * out -возвращает наружу, ковариантность - больше вариантов, более частное
-        //* in -принимает внутрь, контрвариантность - меньше вариантов, более общее
-
+        User tom = new User { Name = "Tom", Auto = new Auto { Name = "Ford" } };
+        int age = 34;
+        var student = new { tom.Auto.Name, age };
+        Console.WriteLine(student.Name);
         Console.Read();
     }
-
-
 }
