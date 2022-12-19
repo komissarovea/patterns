eb3984c13e3d10871dbe88c4b658139fcdb54eda
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 14:11:54 +0300

    enum trick

--- Train/Train/Program.cs
+++ Train/Train/Program.cs
@@ -1,13 +1,17 @@
-﻿void GetRectangleData(in int width, in int height, out int rectArea, out int rectPerimetr)
+﻿enum Day
 {
-    //width = 25; // нельзя изменить, так как width - входной параметр
-    rectArea = width * height;
-    rectPerimetr = (width + height) * 2;
+    Monday = 5,
+    Tuesday,
+    Wednesday,
+    Thursday = 14,
+    Friday = Tuesday,
+    Saturday,
+    Sunday
 }
-
-int w = 10;
-int h = 20;
-GetRectangleData(w, h, out var area, out var perimetr);
-
-Console.WriteLine($"Площадь прямоугольника: {area}");       // 200
-Console.WriteLine($"Периметр прямоугольника: {perimetr}");   // 60
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine((int)Day.Sunday);
+    }
+}
