0a83f72401ffc8f8889713f03784c4b8fb624333
Komissarov Eugene <komissarovea@hotmail.com>
2022-12-19 20:41:50 +0300

    public sealed override void Display()

--- Train/Train/Program.cs
+++ Train/Train/Program.cs
@@ -1,11 +1,45 @@
-﻿class Program
+﻿
+class Person
+{
+    public string Name { get; set; }
+    public virtual void Display()
+    {
+        Console.WriteLine($"Person {Name}");
+    }
+}
+
+class Employee : Person
+{
+    public string Company { get; set; }
+    public sealed override void Display()
+    {
+        Console.WriteLine($"Employee {Name}");
+    }
+}
+
+
+sealed class Manager : Employee
+{
+    public new void Display()
+    {
+        Console.WriteLine($"Manager {Name}");
+    }
+
+ 
+}
+class Program
 {
     static void Main(string[] args)
     {
-        int a = 25;
-        int b = 32;
+        int[] array = new int[33];
 
-        Console.WriteLine(a < 29 ^ b < 29);
+        Person person = new Manager { Name = "Bob", Company = "Microsoft" };
+        person.Display();
+        Employee employee = person as Employee;
+        employee.Display();
+
+        Manager manager = person as Manager;
+        manager.Display();
 
         Console.ReadKey();
     }
